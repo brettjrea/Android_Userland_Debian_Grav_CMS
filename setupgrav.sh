4f038e5121e883a9623017758993123aa2058875
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-05 13:54:59 -0500

    zip

--- setupgrav.sh
+++ setupgrav.sh
@@ -3,17 +3,17 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove -y
 ### add git and php
-sudo apt install git php-common php-curl php-gd php-json php-mbstring php-session php-xml php-zip -y
+sudo apt install git php-cli php-common php-curl php-gd php-json php-mbstring php-session php-xml php-zip unzip zip -y
 ### fix git
 git config --global http.sslverify "false"
 ### install php composer
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php composer-setup.php
-sudo php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
 ### clone grav repo 
-sudo git clone -b master https://github.com/getgrav/grav.git
+git clone -b master https://github.com/getgrav/grav.git
 ### move to grav directory
 cd ~/grav
 ### install dependecies
